35da6c232430ba5fef02fb809eb9c513c56e6837
Eric Wilson <wilsones1991@gmail.com>
2022-08-04 15:47:11 -0700

    Added date endpoint to api

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+const compression = require('compression')
+const helmet = require('helmet')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -12,6 +14,9 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+app.use(compression())
+app.use(helmet())
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -24,7 +29,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date", (req, res) => {
+  if (new Date(req.params.date).toString() === "Invalid Date") {
+    res.json({"error": "Invalid Date"})
+    return
+  }
+  res.json({"unix": Number(new Date(req.params.date)), "utc": new Date(req.params.date).toUTCString()})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
